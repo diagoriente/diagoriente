d377d6d4bc668509d5847146655d31d9985bb4cf
Mérill Téterel <merill.teterel@id6tm.org>
2026-06-15 21:08:43 +0000

    feat: add PostgreSQL setup to apphost

--- apphost.cs
+++ apphost.cs
@@ -1,5 +1,9 @@
+﻿#:package Aspire.Hosting.PostgreSQL@13.4.4
 #:sdk Aspire.AppHost.Sdk@13.4.4
 
 var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("pgsql").WithDataVolume(isReadOnly: false).WithPgAdmin();
+var db = postgres.AddDatabase("diagoriente");
 
 builder.Build().Run();
