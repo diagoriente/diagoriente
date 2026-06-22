4e43519baff123443b4c03166e073e6894b4f4ba
Mérill Téterel <merill.teterel@id6tm.org>
2026-06-22 22:21:02 +0200

    feat: add commitlint-config-moon

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,15 @@
-import type { UserConfig } from '@commitlint/types';
+import { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
+import moon from '@diagoriente/commitlint-config-moon';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [...moon.utils.getProjects(), 'deps', 'release'],
+    ],
+  },
 };
 
 export default Configuration;
---
+++ packages/commitlint-config-moon/index.ts
@@ -0,0 +1,23 @@
+import { RuleConfigSeverity } from '@commitlint/types';
+import { execSync } from 'node:child_process';
+
+function getProjects() {
+  try {
+    const output = execSync('moon projects --json', {
+      encoding: 'utf-8',
+      stdio: ['pipe', 'pipe', 'ignore'],
+    });
+    const projects = JSON.parse(output);
+    return projects.map((p: any) => p.id);
+  } catch {
+    console.warn('⚠️ [commitlint-config-moon] Failed to fetch moon projects.');
+    return [];
+  }
+}
+
+export default {
+  utils: { getProjects },
+  rules: {
+    'scope-enum': [RuleConfigSeverity.Error, 'always', [...getProjects()]],
+  },
+};
