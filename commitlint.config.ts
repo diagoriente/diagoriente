85af7998718f4bbbabeaf7c71498b11dbfbb5c71
Mérill Téterel <merill.teterel@id6tm.org>
2026-06-30 14:44:34 +0200

    fix: lazily set scope-enum in commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,7 +4,7 @@ import moon from '@diagoriente/commitlint-config-moon';
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [
+    'scope-enum': (_ctx) => [
       RuleConfigSeverity.Error,
       'always',
       [...moon.utils.getProjects(), 'deps', 'release'],
