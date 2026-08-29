4ca9420c3fb2c4394a9ce21c0348566eed7e6604
Mérill Téterel <merill.teterel@id6tm.org>
2026-08-29 22:24:00 +0200

    feat(api): setup openapi with scalar ui

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { apiReference } from "@scalar/nestjs-api-reference";
 import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("", app, documentFactory, {
+    ui: false,
+    jsonDocumentUrl: "openapi.json",
+    yamlDocumentUrl: "openapi.yaml",
+  });
+
+  app.use("/docs", apiReference({ url: "/openapi.json" }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
